a3ba345712044d33f12f971bd749e538e601d48e
Josh Garton <jwg054000@gmail.com>
2025-07-22 09:57:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new Streamlit app")
+st.title("🎈 My new https://blank-app-hebm2mbukq.streamlit.app/ app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
